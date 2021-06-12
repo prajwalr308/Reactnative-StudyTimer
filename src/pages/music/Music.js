ee3afea4e4fa5161b95718679bdc28e1e74cb8fc
prajwalr308 <prajwalr.thedeveloper@gmail.com>
2021-06-12 18:32:46 +0530

    audio player

--- src/pages/music/Music.js
+++ src/pages/music/Music.js
@@ -1,15 +1,46 @@
 import React, { useState } from 'react'
-import { View, Text, TouchableOpacity, Image } from 'react-native'
-
+import { ScrollView,View, Text, TouchableOpacity, Image ,Button} from 'react-native'
+import { Audio } from 'expo-av';
 
 
 const Music = () => {
+  const [sound, setSound] = React.useState();
+
+  async function playSound() {
+    console.log('Loading Sound');
+    const { sound } = await Audio.Sound.createAsync(
+       require('../../audio/eg.mp3')
+    );
+    setSound(sound);
+
+    console.log('Playing Sound');
+    await sound.playAsync(); }
+  async function pauseSound() {
+    // console.log('Loading Sound');
+    // const { sound } = await Audio.Sound.createAsync(
+    //    require('../../audio/eg.mp3')
+    // );
+    // setSound(sound);
+
+    // console.log('Playing Sound');
+    await sound.pauseAsync(); }
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync(); }
+      : undefined;
+  }, [sound]);
 
     return (
-        <View >
+      <ScrollView>
+ <Button title="Play Sound" onPress={playSound} />
+ <Button title="pauuse Sound" onPress={pauseSound} />
+      </ScrollView>
     
         
-      </View>
+
     )
 }
 
